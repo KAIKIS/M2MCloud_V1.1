809667bd70cb00617420ad23353d5283aa95b94b
KAIKIS <zhkai0427@gmail.com>
2016-04-17 09:35:26 +0800

    update

--- src/io/m2mcloud/www/collections/Users.java
+++ src/io/m2mcloud/www/collections/Users.java
@@ -1,9 +1,22 @@
 package io.m2mcloud.www.collections;
 
+import java.util.Date;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexed;
+
+@Entity(value = "users", noClassnameStored = true)
 public class Users {
 
-	public static void main(String[] args) {
-		System.out.println("This is a test");
-	}
+	@Id
+	private ObjectId id;
+	private String tokenId;
+	
+	@Indexed(options = @IndexOptions(unique = true))
+	private String userName;
+	private Date createTime;
 	
 }
